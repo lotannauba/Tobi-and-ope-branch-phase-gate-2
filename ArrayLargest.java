d896bde04d81c01660ce47f0d410022c3f03bab8
lotannauba <lotannauba23@gmail.com>
2025-05-29 17:01:17 +0100

    Add files via upload

---
+++ ArrayLargest.java
@@ -0,0 +1,25 @@
+public class ArrayLargest{
+	public static void main(String[]args){
+
+	int index = 1;
+	int largest = 0;
+
+	int[] num = {1,5,3,4,5,5};
+	 
+	for(int counter = 1; counter < num.length; counter++){
+
+		if(largest < num[counter] && num[counter]!= largest)
+		
+	System.out.print(index);	
+	
+				}	
+
+
+
+
+
+	
+		}
+
+
+	}
---
+++ StringArray.java
@@ -0,0 +1,32 @@
+ public class StringName{
+
+	public static void main(String...args){
+	string name1 = word.toLowerCase();
+
+	word = " hello world "
+	Char letters = '0'
+
+
+
+
+for(int counter = 0; counter < word.length; counter++)
+
+	if(name1.charAt() = word){
+		string counter++;
+		}
+
+
+	return Stringcount++;
+
+
+
+
+
+
+			
+
+
+		}
+
+
+	}
