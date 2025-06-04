4b55c9dbd2d0fc297415db8bccd897b454a65bc1
lotannauba <lotannauba23@gmail.com>
2025-06-04 17:02:10 +0100

    Add files via upload

---
+++ Payroll.java
@@ -0,0 +1,227 @@
+import java.util.Scanner;
+public class Payroll{
+	public static void main(String[]args){
+
+	Scanner lotanna = new Scanner(System.in);
+
+
+
+
+int hours = 0;
+int grossPay = 0;
+int payRate = 0;
+			System.out.println("-------------------Welcome to Semicolon Employee Payroll-----------------");	
+			
+			System.out.println("----------------Employee 1 should put his|her info here! ------------------------");
+	
+	
+	
+			System.out.println("Enter your Name: ");
+
+			String name = lotanna.nextLine();
+
+			System.out.printf("Your name is %s", name);
+
+
+
+
+		
+		System.out.println("Enter number of hours worked in a week here : ");
+			double number = lotanna.nextDouble();
+
+
+
+
+			System.out.println("Enter your gross rate here : ");
+			double number2 = lotanna.nextDouble();
+
+     
+
+			System.out.println("Enter your hourly rate here : ");
+				double number3 = lotanna.nextDouble();
+
+
+
+			System.out.println("Enter your federal tax withdrawal rate here : ");
+				double number4 = lotanna.nextDouble();
+				
+
+			System.out.println("Enter your state tax withdrawal rate here : ");
+				double number5 = lotanna.nextDouble();
+			
+
+			System.out.println("Enter your commission rate here : ");
+				double Number6 = lotanna.nextDouble();
+
+
+		System.out.print("--------------------Welcome to Semicolon Employee Payroll-----------------");
+		System.out.println("----------------Employee 2 should put his|her info here! -----------------------");
+
+
+
+			System.out.println("Enter your Name: ");
+
+			String name1 = lotanna.nextLine();
+
+			System.out.printf("Your name is %s", name1);
+
+
+		
+		System.out.println("Enter the number of hours worked in a week here : ");
+				Double var = lotanna.nextDouble();
+
+
+			System.out.println("Enter your gross rate here : ");
+				Double var1 = lotanna.nextDouble();
+
+    
+
+			System.out.println("Enter your hourly rate here : ");
+				Double var2 = lotanna.nextDouble();
+
+
+
+			System.out.println("Enter your federal tax withdrawal rate here : ");
+				Double var3 = lotanna.nextDouble();
+				
+
+			System.out.println("Enter your state tax withdrawal rate here : ");
+				Double var4 = lotanna.nextDouble();
+			
+
+			System.out.println("Enter your commission rate here : ");
+				Double var5 = lotanna.nextDouble();
+
+			
+
+
+			System.out.print("--------------Welcome to Semicolon Employee Payroll-----------------");
+		System.out.println("--------------------Employee 3 should put his|her info here! -----------------------");
+
+
+
+			System.out.println("Enter your Name: ");
+
+			String name2 = lotanna.nextLine();
+
+			System.out.printf("Your name is %s", name1);
+
+
+		
+		System.out.println("Enter the number of hours worked in a week here : ");
+				Double Var6 = lotanna.nextDouble();
+
+
+			System.out.println("Enter your gross rate here : ");
+				Double var7 = lotanna.nextDouble();
+
+    
+
+			System.out.println("Enter your hourly rate here : ");
+				Double var8 = lotanna.nextDouble();
+
+
+
+			System.out.println("Enter your federal tax withdrawal rate here : ");
+				Double var9= lotanna.nextDouble();
+				
+
+			System.out.println("Enter your state tax withdrawal rate here : ");
+				Double var10 = lotanna.nextDouble();
+			
+
+			System.out.println("Enter your commission rate here : ");
+				Double var11 = lotanna.nextDouble();
+
+			
+
+	System.out.print("-------------------------Welcome to Semicolon Employee Payroll-----------------");
+		System.out.println("---------------Employee 4 should put his|her info here!-------------------------");
+
+
+
+			System.out.println("Enter your Name: ");
+
+			String name3 = lotanna.nextLine();
+
+			System.out.printf("Your name is %s", name1);
+
+
+		
+		System.out.println("Enter the number of hours worked in a week here : ");
+				Double var12 = lotanna.nextDouble();
+
+
+			System.out.println("Enter your gross rate here : ");
+				Double var13 = lotanna.nextDouble();
+
+    
+
+			System.out.println("Enter your hourly rate here : ");
+				Double var14 = lotanna.nextDouble();
+
+
+
+			System.out.println("Enter your federal tax withdrawal rate here : ");
+				Double var15 = lotanna.nextDouble();
+				
+
+			System.out.println("Enter your state tax withdrawal rate here : ");
+				Double var16 = lotanna.nextDouble();
+			
+
+			System.out.println("Enter your commission rate here : ");
+				Double var17 = lotanna.nextDouble();
+
+
+
+    
+
+			
+
+	System.out.print("------------------------Welcome to Semicolon Employee Payroll-------------------------");
+		System.out.println("--------------Employee 5 should put his|her info here!-----------------------");
+
+
+
+			System.out.println("Enter your Name: ");
+
+			String name4 = lotanna.nextLine();
+
+			System.out.printf("Your name is %s", name1);
+
+
+		
+		System.out.println("Enter the number of hours worked in a week here : ");
+				Double var18 = lotanna.nextDouble();
+
+
+			System.out.println("Enter your gross rate here : ");
+				Double var19 = lotanna.nextDouble();
+
+    
+
+			System.out.println("Enter your hourly rate here : ");
+				Double var20 = lotanna.nextDouble();
+
+
+
+			System.out.println("Enter your federal tax withdrawal rate here : ");
+				Double var21 = lotanna.nextDouble();
+				
+
+			System.out.println("Enter your state tax withdrawal rate here : ");
+				Double var22 = lotanna.nextDouble();
+			
+
+			System.out.println("Enter your commission rate here : ");
+				Double var23 = lotanna.nextDouble();
+
+
+
+			}
+
+
+
+
+		}
